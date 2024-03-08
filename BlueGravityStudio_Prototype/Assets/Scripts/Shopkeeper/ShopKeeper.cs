a4bbb7cfd9705d313c201a70d0bf9a479635ec62
UltraAlucha <87477405+UltraAlucha@users.noreply.github.com>
2024-03-08 12:57:31 +0400

    Improved Shopkeeper Code

--- BlueGravityStudio_Prototype/Assets/Scripts/Shopkeeper/ShopKeeper.cs
+++ BlueGravityStudio_Prototype/Assets/Scripts/Shopkeeper/ShopKeeper.cs
@@ -1,23 +1,33 @@
+using System;
 using UnityEngine;
 
 public class ShopKeeper : MonoBehaviour
 {
-    [SerializeField] private GameObject _shopPanel;
+    public Action PlayerEntered;
+    public Action PlayerExited;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<Player>(out var player))
         {
-            _shopPanel.gameObject.SetActive(player != null);
+            OnPlayerEntered();
         }
     }
-
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out var player))
         {
-            _shopPanel.gameObject.SetActive(player == null);
+            OnPlayerExited();
         }
+    }
+    private void OnPlayerEntered()
+    {
+        PlayerEntered?.Invoke();
+    }
+
+    private void OnPlayerExited()
+    {
+        PlayerExited?.Invoke();
     }
 }
